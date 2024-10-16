7799870ebcf39fe6eb3dcbdb8ae315bb71e24812
Daniel Fernández <atom52025@gmail.com>
2024-10-16 02:03:12 +0200

    Deployment fixes

--- src/app/api/signalRequest.js
+++ src/app/api/signalRequest.js
@@ -1,9 +1,14 @@
 export const dynamic = 'force-dynamic'; // defaults to auto
 
-const EXTERNAL_API_URL = process.env.NEXT_PUBLIC_API_URL;
+const INTERNAL_API_URL = process.env.NEXT_PUBLIC_INTERNAL_API_URL;
+const EXTERNAL_API_URL = process.env.NEXT_PUBLIC_EXTERNAL_API_URL;
+
+// Detects if its executing on client or in server
+const API_URL = typeof window === 'undefined' ? INTERNAL_API_URL : EXTERNAL_API_URL;
+
 export async function GETSIGNAL(url, token, signal) {
   // Create Query String
-  const completeUrl = `${EXTERNAL_API_URL}/${url}`;
+  const completeUrl = `${API_URL}/${url}`;
 
   // Fetch Data
   const res = await fetch(completeUrl, {
